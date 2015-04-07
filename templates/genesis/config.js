c90811b03cc7575adceeba12c89a5dcbd3de2d8c
Gianpaulo M. Soares <jam_pow@hotmail.com>
2015-04-06 23:51:21 -0300

    adding sample of template help

--- templates/genesis/config.js
+++ templates/genesis/config.js
@@ -12,5 +12,13 @@ module.exports = {
 	"tips" : [
 		"Dont forget to put the module's config inside the array in /config/modules.js",
 		"Good luck, you'll need"
+	],
+	"help" : [
+		"This will be showed when passed the --help flag."
+		"Each array item will be displayed in a new line.",
+		"",
+		"You can pass a blank item for a better organization of your help.",
+		"",
+		"Describe each parameter that should be passed to this generator."
 	]
 }
