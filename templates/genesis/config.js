11f71eba108f3351c783b1c784e737c1d3b706fb
Gianpaulo <jam_pow@hotmail.com>
2015-04-07 23:17:05 -0300

    taking notes of an idea

--- templates/genesis/config.js
+++ templates/genesis/config.js
@@ -13,12 +13,17 @@ module.exports = {
 		"Dont forget to put the module's config inside the array in /config/modules.js",
 		"Good luck, you'll need"
 	],
-	"help" : [
+	"doc" : [
 		"This will be showed when passed the --help flag."
 		"Each array item will be displayed in a new line.",
 		"",
 		"You can pass a blank item for a better organization of your help.",
 		"",
 		"Describe each parameter that should be passed to this generator."
-	]
+	],
+	"helpers" : {
+		"date" : function(){
+			return new Date('dd-mm-yyyy');
+		}
+	}
 }
