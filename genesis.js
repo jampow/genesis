f5e80cf87e628c9002d4cf0fc18baf89c54297b5
Gianpaulo <gsoares@uolinc.com>
2016-04-01 18:25:59 -0300

    Fixing the doc's generator flag

--- genesis.js
+++ genesis.js
@@ -5,16 +5,19 @@ var genesis = require('./lib/genesis.js');
 
 program
 	.version('1.0.0')
-	.option('-l, --list', 'Show help');
+	.option('-l, --list', 'Show help')
+	.option('-d, --doc', 'Show generator doc');
 
-program.on('--help', function() {
-	console.log(program.args);
-});
+//program.on('--help', function() {
+	//console.log(program.args);
+//});
 
 program.parse(process.argv);
 
 if(program.list) {
 	genesis.list();
+} else if(program.doc) {
+	genesis.help(program.args);
 } else {
 	genesis.init(program.args);
 }
